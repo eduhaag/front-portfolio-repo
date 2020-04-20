62c63e1db63a423dff3e08e47f50fab2d8e048ae
Eduardo Felipe Haag <eduardof.haag@gmail.com>
2020-04-20 08:26:34 -0300

    Add READEME.md

---
+++ README.md
@@ -0,0 +1,51 @@
+<p align="center">
+  <img src="https://ik.imagekit.io/dkdai9mqcy/logo_Projects_Repository_tt0lg2p01.png" /><br>
+
+  <img src="https://img.shields.io/github/languages/top/eduhaag/front-portfolio-repo?color=%237159c1" />
+  <img src="https://img.shields.io/github/license/eduhaag/portfolio_repos?color=%237159c1">
+  <img src="https://img.shields.io/github/repo-size/eduhaag/front-portfolio-repo?color=%237159c1" /><br>
+</p>
+<p align="center">
+  <a href="#--about">About</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="-technologies">Technologies</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-how-to-use">How to use</a>
+</p><br>
+
+<img src="https://ik.imagekit.io/dkdai9mqcy/Peek_19-04-2020_21-01_WmWbRQ06O.gif" />
+
+---
+
+# 📝  About
+<p>This project was developed as an exercise for Rocketseat's GoStack Bootcamp.</p>
+<p>Its objective is to provide a web interface that allows the insertion and exclusion of projects in a repository, thus allowing assimilation of the first concepts of React.js</p>
+<p>This project consumes the API developed in the previous exercise</p>
+
+---
+
+# 🚀 Technologies
+This project was developed using the [template](https://github.com/Rocketseat/gostack-template-conceitos-reactjs) provided by [Rocketseat](https://github.com/Rocketseat).<br>
+The technologies used were:
+
+- [Axios](https://github.com/axios/axios)
+- [React.js](https://reactjs.org)
+
+---
+
+# ⚙ How to use
+To clone and run this application, you'll need [Git](https://git-scm.com/), [Node.js](https://nodejs.org/) + [Yarn v1.22](https://yarnpkg.com/) or higher installed on your computer and is required these [API](https://github.com/eduhaag/portfolio_repos).<br/>
+ From your command line:
+ ```bash
+# Clone this repository
+$ git clone https://github.com/eduhaag/portfolio_repos.git
+
+# Go into the repository
+$ cd front-portfolio_repo
+
+# Install dependencies
+$ yarn
+
+# Run the app
+$ yarn start
+ ```
+---
+↪  This project was developed by [Eduardo F. Haag](https://github/eduhaag) using the [template](https://github.com/Rocketseat/gostack-template-conceitos-nodejs) provided by [Rocketseat](https://github.com/Rocketseat).
--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { useEffect, useState } from "react";
 
+import { AiFillLike } from 'react-icons/ai'
+
 import api from './services/api'
 
 import "./styles.css";
 
 function App() {
+  const [newRepository, setNewRepository] = useState({
+    title: '',
+    url: '',
+    techs: ''
+  })
   const [repositories, setRepository] = useState([])
 
   useEffect(() => {
@@ -13,17 +20,43 @@ function App() {
     });
   }, []);
 
-  async function handleAddRepository() {
+  function handleTitleChange(e) {
+    setNewRepository({
+      title: e.target.value,
+      url: newRepository.url,
+      techs: newRepository.techs
+    })
+  }
+
+  function handleUrlChange(e) {
+    setNewRepository({
+      title: newRepository.title,
+      url: e.target.value,
+      techs: newRepository.tech
+    })
+  }
+  function handleTechsChange(e) {
+    setNewRepository({
+      title: newRepository.title,
+      url: newRepository.url,
+      techs: e.target.value
+    })
+  }
+
+  async function handleAddRepository(e) {
+    e.preventDefault()
     const response = await api.post('/repositories', {
-      title: 'bootcamp-gostack-desafios',
-      url: 'https://github.com/Rocketseat/bootcamp-gostack-desafios/tree/master/desafio-conceitos-reactjs',
-      techs: [
-        'reactJS',
-        'nodeJS'
-      ]
+      title: newRepository.title,
+      url: newRepository.url,
+      techs: newRepository.techs ? newRepository.techs.replace(' ', '').split(',') : ' '
     })
 
     setRepository([...repositories, response.data])
+    setNewRepository({
+      title: '',
+      url: '',
+      techs: ''
+    })
   }
 
   async function handleRemoveRepository(id) {
@@ -33,20 +66,38 @@ function App() {
   }
 
   return (
-    <div>
-      <ul data-testid="repository-list">
-        {repositories.map(repository => (
-          <li key={repository.id}>
-            {repository.title}
-            <button onClick={() => handleRemoveRepository(repository.id)}>
-              Remover
-          </button>
-          </li>
-        ))}
-      </ul>
-
-      <button onClick={handleAddRepository}>Adicionar</button>
-    </div>
+    <>
+      <header>
+        <img alt="Projects Repository" src="https://ik.imagekit.io/dkdai9mqcy/logo_Projects_Repository_branco_zvoVRE_Nc.png" />
+        <form>
+          <div>
+            <input type="text" placeholder="Titulo do projeto" value={newRepository.title} onChange={handleTitleChange} />
+            <input type="text" placeholder="Url do repositorio GitHub" value={newRepository.url} onChange={handleUrlChange} />
+          </div>
+          <input type="text" id="tech" placeholder="Tecnologias empregadas (separar por virgula)" value={newRepository.techs} onChange={handleTechsChange} />
+          <button id="add" onClick={e => handleAddRepository(e)}>Adicionar</button>
+        </form>
+      </header>
+      <div>
+        <ul data-testid="repository-list">
+          {repositories.map(repository => (
+            <li key={repository.id}>
+              <div>
+                <h1><a href={repository.url}>{repository.title}</a></h1>
+                <span>Techs: {JSON.stringify(repository.techs).replace(",", ", ").replace(/.*\[|\]|"/g, '')}</span>
+                <span id="like">
+                  <AiFillLike color="#ec1868" size={24} />
+                  <p> {`${repository.likes}`}</p>
+                </span>
+              </div>
+              <button onClick={() => handleRemoveRepository(repository.id)}>
+                Remover
+            </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 }
 
